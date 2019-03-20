b19c0c8e06326743642670dac5ac201f31ee4c19
Casey <caseyclinga@gmail.com>
2019-03-19 18:11:36 -0700

    3.19 update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,15 @@ $(document).ready(function() {
 The player will be shown a random number at the start of the game.
 ---When page loads, generate a random number 19 - 129 for player's number*/
     
-    var randomNumber = Math.floor(Math.random()*111) + 19;
-    console.log(randomNumber);
-    
+    function generateRandomNum() {
+    var playerNumber = Math.floor(Math.random()*111) + 19;
+    console.log(playerNumber);
+    console.log(typeof(playerNumber));
+    return playerNumber;
+    }
+
+    var randomNumber = generateRandomNum();
+
 //---Variable to hold player's score
     var playerScore = 0;
 
@@ -35,7 +41,6 @@ The player will be shown a random number at the start of the game.
     $("#crystal2").val(generateCrystalNum());
     $("#crystal3").val(generateCrystalNum());
     $("#crystal4").val(generateCrystalNum());
-
     console.log(typeof($("#crystal1").val()));
     console.log($("#crystal1").val());
 
@@ -80,7 +85,7 @@ The player will be shown a random number at the start of the game.
         if(randomNumber === playerScore) {
             $("#score").html(playerScore = 0);
             $("#wins").html(winCounter += 1);
-            $("#number").html(randomNumber);
+            $("#number").html(generateRandomNumber());
             alert("You win!");
             console.log(winCounter);
             $("#crystal1").val(generateCrystalNum());
@@ -92,10 +97,11 @@ The player will be shown a random number at the start of the game.
 //The loss counter increases by one, the player score gets set back to zero
 //A new random number is generated
 //The crystals are given new random values
+
         } else if(playerScore > randomNumber) {
             $("#score").html(playerScore = 0);
             $("#losses").html(lossCounter += 1);
-            $("#number").html(randomNumber);
+            $("#number").html(generateRandomNum());
             alert("You lose!");
             console.log(lossCounter);
             $("#crystal1").val(generateCrystalNum());
@@ -104,15 +110,4 @@ The player will be shown a random number at the start of the game.
             $("#crystal4").val(generateCrystalNum());
         }
     }
-//The player loses if their score goes above the random number.
-//---If the score is greater than the random number, than the player loses and the loss counter goes up by one
-//---Else, if the score is equal to the random number, than the player wins and the win counter goes up by one
-
-//The game restarts whenever the player wins or loses.
-//---When the score is greater than or equal to the random number, restart the game
-
-//When the game begingenes again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-//---After restarting the game, generate a new random number and new random values for the crystals
-
-//The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game. */
 })
